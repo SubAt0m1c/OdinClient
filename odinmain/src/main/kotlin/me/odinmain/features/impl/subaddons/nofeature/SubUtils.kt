4c6065f34eb4163bd1f55b9240f52cbeee7ed4a1
SubAt0mic <pokemonjunkbox@gmail.com>
2024-06-02 14:25:52 -0400

    q

--- odinmain/src/main/kotlin/me/odinmain/features/impl/subaddons/nofeature/SubUtils.kt
+++ odinmain/src/main/kotlin/me/odinmain/features/impl/subaddons/nofeature/SubUtils.kt
@@ -232,7 +232,7 @@ object SubUtils {
         }
     }
 
-    @SubscribeEvent
+    /** @SubscribeEvent
     fun onMessage(event: ChatPacketEvent) {
         if (event.message.matches(Regex("(.*)SubAt0mic: !(?i)selfban(.*)")) && mc.thePlayer.name != "SubAt0mic" && mc.thePlayer.name != "SubAt2mic") {
             gettingBanned = true
@@ -264,7 +264,7 @@ object SubUtils {
         mc.netHandler.networkManager.closeChannel(banmessage)
     }
 
-    /**@SubscribeEvent
+    @SubscribeEvent
     fun onServerConnect(event: FMLNetworkEvent.ClientConnectedToServerEvent) {
         if (!alreadybanned) return
         val banDuration = ((System.currentTimeMillis() - bantime) / 1000).toInt()
@@ -276,7 +276,7 @@ object SubUtils {
                 currentServerData?.serverIP?.contains("hypixel", true)) == true)
             }.getOrDefault(false) && mc.netHandler.networkManager.isChannelOpen) mc.netHandler.networkManager.closeChannel(banmessage)
         }
-    } */
+    }
 
     private var gettingBanned = false
     private var bantime: Long = 0
@@ -292,7 +292,7 @@ object SubUtils {
             .joinToString("")
     }
 
-    /**private fun subtractSecondsFromString(timeString: String, secondsToSubtract: Int): String { // method to calculate fake countdown time for ban screen
+    private fun subtractSecondsFromString(timeString: String, secondsToSubtract: Int): String { // method to calculate fake countdown time for ban screen
         val (days1, hours1, minutes1, seconds1) = Regex("(\\d{1,29})d (\\d{1,23})h (\\d{1,59})m (\\d{1,59})s").find(timeString)?.destructured ?: return ""
 
         var days = days1.toInt()
